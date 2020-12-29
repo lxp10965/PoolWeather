5ea0767a4acb19e38d40df90f076166d34b1e168
xpl <1096589897@qq.com>
2020-12-29 18:56:54 +0800

    加fragment

--- app/src/main/java/com/xpl/poolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/xpl/poolweather/ChooseAreaFragment.java
@@ -6,9 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChooseAreaFragment extends Fragment {
+
+
+    private View titleText;
+    private View backButton;
+    private View listView;
+    private ArrayAdapter adapter;
+    private List<String> dataList=new ArrayList<>();
 
 
     @Override
@@ -19,7 +30,13 @@ public class ChooseAreaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.choose_area, container, false);
+
+        View view = inflater.inflate(R.layout.choose_area, container, false);
+        titleText = view.findViewById(R.id.title_text);
+        backButton = view.findViewById(R.id.back_button);
+        listView = view.findViewById(R.id.list_view);
+        adapter=new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+
+        return view;
     }
 }
